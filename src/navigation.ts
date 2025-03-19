d6e50cb55df6cfe2168046beb10efbb81f41ac4a
leighton <leighton@photoshelter.com>
2025-03-18 20:45:12 -0400

    developed by footnote

--- src/navigation.ts
+++ src/navigation.ts
@@ -60,7 +60,10 @@ export const footerData = {
     { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: '#' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    &#9178; 
+    Developed by 
+    <a target="_blank" rel="noopener noreferrer" href="https://github.com/LKWaters"><u>LKWaters</u></a>
+     and 
+    <a target="_blank" rel="noopener noreferrer" href="https://github.com/dzialoj"><u>dizaloj</u></a>
   `,
 };
